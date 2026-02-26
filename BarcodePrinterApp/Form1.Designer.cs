1bfadeddb9a069c1ed9d6683755e7f6921a08e7e
Sibel Akkurt <sibelakk23@gmail.com>
2026-02-26 16:29:19 +0300

    Initial commit with gitignore

---
+++ BarcodePrinterApp/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace BarcodePrinterApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblTedarikci = new Label();
+            openFileDialog1 = new OpenFileDialog();
+            cmbTedarikci = new ComboBox();
+            lblDepo = new Label();
+            cmbDepo = new ComboBox();
+            lblUrun = new Label();
+            cmbUrun = new ComboBox();
+            grpKabulBilgileri = new GroupBox();
+            lblAdet = new Label();
+            lblKondisyon = new Label();
+            lblRafNo = new Label();
+            lblKabulKodu = new Label();
+            numericUpDown1 = new NumericUpDown();
+            comboBox1 = new ComboBox();
+            txtRafNo = new TextBox();
+            txtKabulKodu = new TextBox();
+            btnKaydet = new Button();
+            cmbYazici = new ComboBox();
+            label2 = new Label();
+            button2 = new Button();
+            btnPrn = new Button();
+            grpKabulBilgileri.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // lblTedarikci
+            // 
+            lblTedarikci.AutoSize = true;
+            lblTedarikci.Location = new Point(28, 34);
+            lblTedarikci.Name = "lblTedarikci";
+            lblTedarikci.Size = new Size(68, 20);
+            lblTedarikci.TabIndex = 2;
+            lblTedarikci.Text = "Tedarikçi";
+            lblTedarikci.Click += label1_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            openFileDialog1.FileOk += openFileDialog1_FileOk;
+            // 
+            // cmbTedarikci
+            // 
+            cmbTedarikci.BackColor = Color.White;
+            cmbTedarikci.FormattingEnabled = true;
+            cmbTedarikci.Location = new Point(28, 57);
+            cmbTedarikci.Name = "cmbTedarikci";
+            cmbTedarikci.Size = new Size(223, 28);
+            cmbTedarikci.TabIndex = 3;
+            cmbTedarikci.Text = "Seçiniz";
+            // 
+            // lblDepo
+            // 
+            lblDepo.AutoSize = true;
+            lblDepo.Location = new Point(28, 124);
+            lblDepo.Name = "lblDepo";
+            lblDepo.Size = new Size(46, 20);
+            lblDepo.TabIndex = 4;
+            lblDepo.Text = "Depo";
+            // 
+            // cmbDepo
+            // 
+            cmbDepo.FormattingEnabled = true;
+            cmbDepo.Location = new Point(28, 147);
+            cmbDepo.Name = "cmbDepo";
+            cmbDepo.Size = new Size(223, 28);
+            cmbDepo.TabIndex = 5;
+            cmbDepo.Text = "Seçiniz";
+            // 
+            // lblUrun
+            // 
+            lblUrun.AutoSize = true;
+            lblUrun.Location = new Point(28, 218);
+            lblUrun.Name = "lblUrun";
+            lblUrun.Size = new Size(40, 20);
+            lblUrun.TabIndex = 6;
+            lblUrun.Text = "Ürün";
+            // 
+            // cmbUrun
+            // 
+            cmbUrun.FormattingEnabled = true;
+            cmbUrun.Location = new Point(28, 240);
+            cmbUrun.Name = "cmbUrun";
+            cmbUrun.Size = new Size(223, 28);
+            cmbUrun.TabIndex = 7;
+            cmbUrun.Text = "Seçiniz";
+            // 
+            // grpKabulBilgileri
+            // 
+            grpKabulBilgileri.BackColor = Color.White;
+            grpKabulBilgileri.Controls.Add(btnKaydet);
+            grpKabulBilgileri.Controls.Add(txtKabulKodu);
+            grpKabulBilgileri.Controls.Add(txtRafNo);
+            grpKabulBilgileri.Controls.Add(comboBox1);
+            grpKabulBilgileri.Controls.Add(numericUpDown1);
+            grpKabulBilgileri.Controls.Add(lblKabulKodu);
+            grpKabulBilgileri.Controls.Add(lblRafNo);
+            grpKabulBilgileri.Controls.Add(lblKondisyon);
+            grpKabulBilgileri.Controls.Add(lblAdet);
+            grpKabulBilgileri.Controls.Add(cmbUrun);
+            grpKabulBilgileri.Controls.Add(lblTedarikci);
+            grpKabulBilgileri.Controls.Add(cmbTedarikci);
+            grpKabulBilgileri.Controls.Add(lblDepo);
+            grpKabulBilgileri.Controls.Add(cmbDepo);
+            grpKabulBilgileri.Controls.Add(lblUrun);
+            grpKabulBilgileri.Location = new Point(16, 12);
+            grpKabulBilgileri.Name = "grpKabulBilgileri";
+            grpKabulBilgileri.Size = new Size(688, 352);
+            grpKabulBilgileri.TabIndex = 8;
+            grpKabulBilgileri.TabStop = false;
+            grpKabulBilgileri.Text = "Kabul Bilgileri";
+            // 
+            // lblAdet
+            // 
+            lblAdet.AutoSize = true;
+            lblAdet.Location = new Point(428, 34);
+            lblAdet.Name = "lblAdet";
+            lblAdet.Size = new Size(41, 20);
+            lblAdet.TabIndex = 8;
+            lblAdet.Text = "Adet";
+            // 
+            // lblKondisyon
+            // 
+            lblKondisyon.AutoSize = true;
+            lblKondisyon.Location = new Point(428, 93);
+            lblKondisyon.Name = "lblKondisyon";
+            lblKondisyon.Size = new Size(78, 20);
+            lblKondisyon.TabIndex = 9;
+            lblKondisyon.Text = "Kondisyon";
+            // 
+            // lblRafNo
+            // 
+            lblRafNo.AutoSize = true;
+            lblRafNo.Location = new Point(428, 156);
+            lblRafNo.Name = "lblRafNo";
+            lblRafNo.Size = new Size(55, 20);
+            lblRafNo.TabIndex = 10;
+            lblRafNo.Text = "Raf No";
+            // 
+            // lblKabulKodu
+            // 
+            lblKabulKodu.AutoSize = true;
+            lblKabulKodu.Location = new Point(428, 218);
+            lblKabulKodu.Name = "lblKabulKodu";
+            lblKabulKodu.Size = new Size(127, 20);
+            lblKabulKodu.TabIndex = 11;
+            lblKabulKodu.Text = "Depo Kabul Kodu";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(428, 57);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(220, 27);
+            numericUpDown1.TabIndex = 12;
+            numericUpDown1.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(428, 116);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(220, 28);
+            comboBox1.TabIndex = 13;
+            comboBox1.Text = "Seçiniz";
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // txtRafNo
+            // 
+            txtRafNo.Location = new Point(428, 179);
+            txtRafNo.Name = "txtRafNo";
+            txtRafNo.Size = new Size(220, 27);
+            txtRafNo.TabIndex = 14;
+            txtRafNo.Text = "Opsiyonel";
+            // 
+            // txtKabulKodu
+            // 
+            txtKabulKodu.Location = new Point(428, 241);
+            txtKabulKodu.Name = "txtKabulKodu";
+            txtKabulKodu.ReadOnly = true;
+            txtKabulKodu.Size = new Size(220, 27);
+            txtKabulKodu.TabIndex = 15;
+            // 
+            // btnKaydet
+            // 
+            btnKaydet.BackColor = Color.LimeGreen;
+            btnKaydet.Location = new Point(490, 299);
+            btnKaydet.Name = "btnKaydet";
+            btnKaydet.Size = new Size(171, 34);
+            btnKaydet.TabIndex = 17;
+            btnKaydet.Text = "Kaydet ve QR Göster";
+            btnKaydet.UseVisualStyleBackColor = false;
+            // 
+            // cmbYazici
+            // 
+            cmbYazici.FormattingEnabled = true;
+            cmbYazici.Location = new Point(420, 393);
+            cmbYazici.Name = "cmbYazici";
+            cmbYazici.Size = new Size(157, 28);
+            cmbYazici.TabIndex = 18;
+            cmbYazici.Text = " Yazıcı Seçiniz";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(203, 371);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 20);
+            label2.TabIndex = 20;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.DodgerBlue;
+            button2.Location = new Point(583, 389);
+            button2.Name = "button2";
+            button2.Size = new Size(111, 32);
+            button2.TabIndex = 21;
+            button2.Text = "Yazdır";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // btnPrn
+            // 
+            btnPrn.BackColor = Color.DodgerBlue;
+            btnPrn.Location = new Point(12, 393);
+            btnPrn.Name = "btnPrn";
+            btnPrn.Size = new Size(185, 29);
+            btnPrn.TabIndex = 22;
+            btnPrn.Text = "PRN Dosyasını Seç";
+            btnPrn.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.WhiteSmoke;
+            ClientSize = new Size(716, 454);
+            Controls.Add(btnPrn);
+            Controls.Add(button2);
+            Controls.Add(label2);
+            Controls.Add(cmbYazici);
+            Controls.Add(grpKabulBilgileri);
+            Name = "Form1";
+            Text = "Depo Kabul";
+            Load += Form1_Load;
+            grpKabulBilgileri.ResumeLayout(false);
+            grpKabulBilgileri.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label lblTedarikci;
+        private OpenFileDialog openFileDialog1;
+        private ComboBox cmbTedarikci;
+        private Label lblDepo;
+        private ComboBox cmbDepo;
+        private Label lblUrun;
+        private ComboBox cmbUrun;
+        private GroupBox grpKabulBilgileri;
+        private TextBox txtKabulKodu;
+        private TextBox txtRafNo;
+        private ComboBox comboBox1;
+        private NumericUpDown numericUpDown1;
+        private Label lblKabulKodu;
+        private Label lblRafNo;
+        private Label lblKondisyon;
+        private Label lblAdet;
+        private Button btnKaydet;
+        private ComboBox cmbYazici;
+        private Label label2;
+        private Button button2;
+        private Button btnPrn;
+    }
+}
---
+++ BarcodePrinterApp/Form1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using QRCoder;
+
+namespace BarcodePrinterApp
+{
+    public partial class Form1 : Form
+    {
+        private string _prnFilePath = string.Empty;
+        private static int _kabulSayac = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnKaydet.Click += btnKaydet_Click;
+            button2.Click += button2_Click;
+            btnPrn.Click += btnPrn_Click;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            YazicilariListele();
+            cmbTedarikci.Items.AddRange(new object[] { "Tedarikcı A", "Tedarikcı B", "Tedarikcı C" });
+            cmbDepo.Items.AddRange(new object[] { "Ana Depo", "Yedek Depo", "Soguk Depo" });
+            cmbUrun.Items.AddRange(new object[] { "Urun 001", "Urun 002", "Urun 003" });
+            comboBox1.Items.AddRange(new object[] { "Sifir", "Ikinci El", "Hasarli" });
+            txtKabulKodu.Text = UretKabulKodu();
+            txtRafNo.ForeColor = Color.Gray;
+            txtRafNo.Enter += TxtRafNo_Enter;
+            txtRafNo.Leave += TxtRafNo_Leave;
+        }
+
+        private void TxtRafNo_Enter(object sender, EventArgs e)
+        {
+            if (txtRafNo.Text == "Opsiyonel")
+            {
+                txtRafNo.Text = "";
+                txtRafNo.ForeColor = Color.Black;
+            }
+        }
+
+        private void TxtRafNo_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtRafNo.Text))
+            {
+                txtRafNo.Text = "Opsiyonel";
+                txtRafNo.ForeColor = Color.Gray;
+            }
+        }
+
+        private void YazicilariListele()
+        {
+            cmbYazici.Items.Clear();
+            foreach (string yazici in PrinterSettings.InstalledPrinters)
+                cmbYazici.Items.Add(yazici);
+            if (cmbYazici.Items.Count > 0)
+                cmbYazici.SelectedIndex = 0;
+        }
+
+        private string UretKabulKodu()
+        {
+            return string.Format("DK{0:D6}", _kabulSayac);
+        }
+
+        private void btnPrn_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "PRN Dosyalari (*.prn)|*.prn|Tum Dosyalar (*.*)|*.*";
+            openFileDialog1.Title = "PRN Dosyasi Sec";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                _prnFilePath = openFileDialog1.FileName;
+                MessageBox.Show("Dosya secildi:\n" + _prnFilePath, "PRN Dosyasi",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(_prnFilePath))
+            {
+                MessageBox.Show("Lutfen once bir PRN dosyasi secin.", "Uyari",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (cmbYazici.SelectedItem == null)
+            {
+                MessageBox.Show("Lutfen bir yazici secin.", "Uyari",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            string yaziciAdi = cmbYazici.SelectedItem.ToString();
+            try
+            {
+                byte[] prnData = File.ReadAllBytes(_prnFilePath);
+                bool sonuc = RawPrintHelper.SendBytesToPrinter(yaziciAdi, prnData);
+                if (sonuc)
+                    MessageBox.Show("Yazdirma islemi baslatildi.", "Basarili",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else
+                    MessageBox.Show("Yazdirma sirasinda bir hata olustu.", "Hata",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message, "Hata",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            if (cmbTedarikci.SelectedItem == null && string.IsNullOrEmpty(cmbTedarikci.Text.Trim()))
+            {
+                MessageBox.Show("Lutfen Tedarikcı secin.", "Uyari",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            string kabulKodu = txtKabulKodu.Text;
+            string qrIcerik = "DEPO_KABUL|" + kabulKodu + "|" + DateTime.Now.ToString("yyyyMMddHHmm");
+
+            using (QRCodeGenerator generator = new QRCodeGenerator())
+            {
+                QRCodeData data = generator.CreateQrCode(qrIcerik, QRCodeGenerator.ECCLevel.M);
+                using (PngByteQRCode qrCode = new PngByteQRCode(data))
+                {
+                    byte[] pngBytes = qrCode.GetGraphic(10);
+                    using (MemoryStream ms = new MemoryStream(pngBytes))
+                    {
+                        Image qrImage = Image.FromStream(ms);
+                        QRForm qrForm = new QRForm(qrImage, kabulKodu);
+                        qrForm.ShowDialog();
+                    }
+                }
+            }
+            _kabulSayac++;
+            txtKabulKodu.Text = UretKabulKodu();
+        }
+
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e) { }
+        private void label1_Click(object sender, EventArgs e) { }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) { }
+    }
+
+    internal static class RawPrintHelper
+    {
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+        private class DOCINFOA
+        {
+            [MarshalAs(UnmanagedType.LPStr)] public string pDocName = "RAW Document";
+            [MarshalAs(UnmanagedType.LPStr)] public string pOutputFile = null;
+            [MarshalAs(UnmanagedType.LPStr)] public string pDataType = "RAW";
+        }
+
+        [DllImport("winspool.Drv", EntryPoint="OpenPrinterA", SetLastError=true, CharSet=CharSet.Ansi, ExactSpelling=true, CallingConvention=CallingConvention.StdCall)]
+        private static extern bool OpenPrinter([MarshalAs(UnmanagedType.LPStr)] string szPrinter, out IntPtr hPrinter, IntPtr pd);
+
+        [DllImport("winspool.Drv", EntryPoint="ClosePrinter", SetLastError=true)]
+        private static extern bool ClosePrinter(IntPtr hPrinter);
+
+        [DllImport("winspool.Drv", EntryPoint="StartDocPrinterA", SetLastError=true, CharSet=CharSet.Ansi, ExactSpelling=true, CallingConvention=CallingConvention.StdCall)]
+        private static extern bool StartDocPrinter(IntPtr hPrinter, Int32 level, [In, MarshalAs(UnmanagedType.LPStruct)] DOCINFOA di);
+
+        [DllImport("winspool.Drv", EntryPoint="EndDocPrinter", SetLastError=true)]
+        private static extern bool EndDocPrinter(IntPtr hPrinter);
+
+        [DllImport("winspool.Drv", EntryPoint="StartPagePrinter", SetLastError=true)]
+        private static extern bool StartPagePrinter(IntPtr hPrinter);
+
+        [DllImport("winspool.Drv", EntryPoint="EndPagePrinter", SetLastError=true)]
+        private static extern bool EndPagePrinter(IntPtr hPrinter);
+
+        [DllImport("winspool.Drv", EntryPoint="WritePrinter", SetLastError=true, ExactSpelling=true, CallingConvention=CallingConvention.StdCall)]
+        private static extern bool WritePrinter(IntPtr hPrinter, IntPtr pBytes, Int32 dwCount, out Int32 dwWritten);
+
+        public static bool SendBytesToPrinter(string printerName, byte[] bytes)
+        {
+            IntPtr hPrinter = IntPtr.Zero;
+            bool bSuccess = false;
+            if (!OpenPrinter(printerName, out hPrinter, IntPtr.Zero)) return false;
+            try
+            {
+                DOCINFOA di = new DOCINFOA();
+                if (!StartDocPrinter(hPrinter, 1, di)) return false;
+                if (!StartPagePrinter(hPrinter)) { EndDocPrinter(hPrinter); return false; }
+                IntPtr pBytes = Marshal.AllocCoTaskMem(bytes.Length);
+                try
+                {
+                    Marshal.Copy(bytes, 0, pBytes, bytes.Length);
+                    Int32 dwWritten = 0;
+                    bSuccess = WritePrinter(hPrinter, pBytes, bytes.Length, out dwWritten);
+                }
+                finally { Marshal.FreeCoTaskMem(pBytes); }
+                EndPagePrinter(hPrinter);
+                EndDocPrinter(hPrinter);
+            }
+            finally { ClosePrinter(hPrinter); }
+            return bSuccess;
+        }
+    }
+
+    internal class QRForm : Form
+    {
+        private readonly Image _qrImage;
+        private readonly string _kabulKodu;
+
+        public QRForm(Image qrImage, string kabulKodu)
+        {
+            _qrImage = qrImage;
+            _kabulKodu = kabulKodu;
+            SetupUI();
+        }
+
+        private void SetupUI()
+        {
+            Text = "QR Kodu - " + _kabulKodu;
+            Size = new Size(380, 460);
+            StartPosition = FormStartPosition.CenterParent;
+            BackColor = Color.White;
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+
+            Label lblBaslik = new Label
+            {
+                Text = "Depo Kabul Kodu: " + _kabulKodu,
+                Font = new Font("Segoe UI", 11, FontStyle.Bold),
+                AutoSize = false,
+                TextAlign = ContentAlignment.MiddleCenter,
+                Dock = DockStyle.Top,
+                Height = 45
+            };
+
+            PictureBox pbQR = new PictureBox
+            {
+                Image = _qrImage,
+                SizeMode = PictureBoxSizeMode.Zoom,
+                Width = 280,
+                Height = 280,
+                Left = 40,
+                Top = 55,
+                BackColor = Color.White
+            };
+
+            Button btnKaydet = new Button
+            {
+                Text = "PNG Olarak Kaydet",
+                Width = 180,
+                Height = 36,
+                Left = 90,
+                Top = 350,
+                BackColor = Color.LimeGreen,
+                ForeColor = Color.White,
+                FlatStyle = FlatStyle.Flat,
+                Font = new Font("Segoe UI", 9, FontStyle.Bold)
+            };
+            btnKaydet.FlatAppearance.BorderSize = 0;
+            btnKaydet.Click += (s, e2) =>
+            {
+                using (SaveFileDialog sfd = new SaveFileDialog
+                {
+                    Filter = "PNG Dosyasi (*.png)|*.png",
+                    FileName = "QR_" + _kabulKodu + ".png"
+                })
+                {
+                    if (sfd.ShowDialog() == DialogResult.OK)
+                    {
+                        _qrImage.Save(sfd.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                        MessageBox.Show("QR kodu kaydedildi.", "Basarili",
+                            MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            };
+
+            Controls.AddRange(new Control[] { lblBaslik, pbQR, btnKaydet });
+        }
+    }
+}
